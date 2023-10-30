25fa25bf0bf8543e3fb3517bf2153844a221666f
javier <i202212336@cibertec.edu.pe>
2023-10-30 02:14:02 -0500

    Se actualiza el proyecto

--- src/main/java/com/cibertec/controller/ControllerP.java
+++ src/main/java/com/cibertec/controller/ControllerP.java
@@ -9,9 +9,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.cibertec.interfaceService.IcategoriaService;
 import com.cibertec.interfaceService.IproductoService;
+import com.cibertec.interfaceService.IproveedorService;
+import com.cibertec.model.Categoria;
 import com.cibertec.model.Producto;
-
+import com.cibertec.model.Proveedor;
 
 import jakarta.validation.Valid;
 
@@ -23,6 +26,12 @@ public class ControllerP {
 	@Autowired
 	private IproductoService service;
 	
+	@Autowired
+	private IcategoriaService serviceCate;
+	
+	@Autowired
+	private IproveedorService serviceProv;
+	
 	@GetMapping("/listar")
 	public String listar(Model model) {
 		List<Producto>productos = service.listar();
@@ -30,13 +39,22 @@ public class ControllerP {
 		return "index";
 	}
 	
+	
+	
+	
+	
+	
 	@GetMapping("/newProducto")
 	public String agregar(Model model) {
 		model.addAttribute("producto", new Producto());
+		List<Categoria>categorias = serviceCate.listar();
+		model.addAttribute("categorias", categorias);
+		List<Proveedor>proveedores = serviceProv.listar();
+		model.addAttribute("proveedores", proveedores);
 		return "formulario";
 	}
-	
 
+	
 	@PostMapping("/guardarProducto")
 	public String save(@Valid Producto p, Model model) {
 		service.save(p);
